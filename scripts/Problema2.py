88dc0c4e589c743c3b2dec31d6efb9709510856d
Marcos <marcoslm@ciencias.unam.mx>
2021-07-28 02:08:26 -0500

    Problema 2

--- scripts/Problema2.py
+++ scripts/Problema2.py
@@ -4,10 +4,9 @@ def characters(string1, string2):
     if 1 <= len(string1) <= 15 and 1 <= len(string2) <= 15:
         asciiletters = set("abcdefghijklmnopqrstuvwxyz")
         if all((i in asciiletters) for i in string1) and  all((i in asciiletters) for i in string2):
-            String1List = list(string1)
             String2List = list(string2)
             c = 0
-            for j in String1List:
+            for j in string1:
                 if j in String2List:
                     c += 1
                     String2List.remove(j)
