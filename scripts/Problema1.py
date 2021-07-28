08d41255c77046c09ca0f2bd4a68ad95ae08507c
Marcos <marcoslm@ciencias.unam.mx>
2021-07-28 12:31:51 -0500

    Problema 1

--- scripts/Problema1.py
+++ scripts/Problema1.py
@@ -1,7 +1,19 @@
 # Problema 1
 
 def acomodo(tablas):
-    Sorttablas = sorted(tablas)
-    NSorttablas = list(range(Sorttablas[0], Sorttablas[-1] + 1))
-    diff = len(NSorttablas) - len(Sorttablas)
-    return diff
+    if 1 <= len(tablas) <= 10:
+        Sorttablas = sorted(tablas)
+        NSorttablas = list(range(Sorttablas[0], Sorttablas[-1] + 1))
+        diff = len(NSorttablas) - len(Sorttablas)
+        return diff
+    else:
+            print("Lista inválida")
+
+
+"""
+La restricción acerca de la unidades máximas puede ser ignorada, ya que el número máximo que acepta Python es:
+
+9223372036854775807.
+
+Es decir, un número de 10^19.
+"""
