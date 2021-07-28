0ac4c451127c06e9a83f27b0560b0618f3978c8f
Marcos <marcoslm@ciencias.unam.mx>
2021-07-28 16:23:03 -0500

    Problema 9. Incisio 9.3

---
+++ scripts/Problema93.py
@@ -0,0 +1,20 @@
+# Problema 9. Incisos 9.3
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.random.normal(50, 15, 1000)
+y = np.random.normal(110, 10, 1000)
+z = np.random.normal(10, 5, 1000)
+NBins = 50
+xc = [10, 50, 110] 
+
+fig, ax = plt.subplots(figsize = (10, 5))
+ax.set_title("Histogramas de distribuciones normales diferentes")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+plt.hist(x, NBins, facecolor = 'deeppink');
+plt.hist(y, NBins, facecolor = 'darkblue');
+plt.hist(z, NBins, facecolor = 'darkred');
+for i in xc:
+    plt.axvline(x = i, color = 'k', linestyle = '--')
+plt.show()
