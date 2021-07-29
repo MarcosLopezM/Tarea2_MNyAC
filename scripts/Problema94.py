5072f5f2d81a2958c048e3ed3c370278b0790aa9
Marcos <marcoslm@ciencias.unam.mx>
2021-07-29 16:22:16 -0500

    Problema 4 incompleto

--- scripts/Problema94.py
+++ scripts/Problema94.py
@@ -2,17 +2,19 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-x = np.random.normal(50, 15, 1000)
-y = np.random.normal(110, 10, 1000)
-z = np.random.normal(10, 5, 1000)
-fx = np.random.normal(50, 15, 1000)
+x = np.random.normal(loc = 10, scale = 5, size = 1000)
+y = np.random.normal(loc = 50, scale = 10, size = 1000)
+z = np.random.normal(loc = 110, scale = 15, size = 1000)
+
+fx, fy, fz = np.random.normal(size = 1000), np.random.normal(size = 1000), np.random.normal(size = 1000)
 NBins = 50 
 
 def normal(data, mu, sigma):
     return (1 / sigma * np.sqrt(2 * np.pi)) * np.exp((-0.5 * (x - mu) ** 2) / sigma ** 2)
 
-fig, ax = plt.subplots(1, 2, figsize = (10, 5))
-ax[0].scatter(x, fx)
-ax[1].scatter(x, normal(x, 50, 15))
+fig, ax = plt.subplots(figsize = (10, 5))
+ax.scatter(x, fx, c = 'k')
+ax.scatter(y, fy, c = 'c')
+ax.scatter(z, fz, c = 'deeppink')
 plt.show()
 
