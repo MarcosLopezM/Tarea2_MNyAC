a92535d57f8042b85d27f448e2c1cbb2f92fa88a
Marcos <marcoslm@ciencias.unam.mx>
2021-07-29 19:58:51 -0500

    Problema 9. Inciso 9.4

--- scripts/Problema94.py
+++ scripts/Problema94.py
@@ -5,16 +5,14 @@ import numpy as np
 x = np.random.normal(loc = 10, scale = 5, size = 1000)
 y = np.random.normal(loc = 50, scale = 10, size = 1000)
 z = np.random.normal(loc = 110, scale = 15, size = 1000)
+indices = list(range(1, 1001))
+colors = ['deeppink', 'darkblue', 'darkred']
 
-fx, fy, fz = np.random.normal(size = 1000), np.random.normal(size = 1000), np.random.normal(size = 1000)
-NBins = 50 
-
-def normal(data, mu, sigma):
-    return (1 / sigma * np.sqrt(2 * np.pi)) * np.exp((-0.5 * (x - mu) ** 2) / sigma ** 2)
 
 fig, ax = plt.subplots(figsize = (10, 5))
-ax.scatter(x, fx, c = 'k')
-ax.scatter(y, fy, c = 'c')
-ax.scatter(z, fz, c = 'deeppink')
+ax.scatter(indices, x, c = colors[0], label = "Dist. 1")
+ax.scatter(indices, y, c = colors[1], label = "Dist. 2")
+ax.scatter(indices, z, c = colors[2], label = "Dist. 3")
+ax.set_title("Gráfica de dispersión de las diferentes distribuciones")
+ax.legend()
 plt.show()
-
